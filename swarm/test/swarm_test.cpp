2df0f339093f4ee96d37ba7a42e3e7d1f21e0f03
Paul Archard <paul@bluzelle.com>
2019-04-24 10:08:27 -0700

    wip

--- swarm/test/swarm_test.cpp
+++ swarm/test/swarm_test.cpp
@@ -58,7 +58,8 @@ protected:
     std::shared_ptr<bzn::asio::Mockio_context_base> mock_io_context = std::make_shared<bzn::asio::Mockio_context_base>();
     std::shared_ptr<crypto_base> crypto = std::make_shared<null_crypto>();
     std::shared_ptr<bzn::beast::websocket_base> ws_factory = std::make_shared<bzn::beast::Mockwebsocket_base>();
-    std::shared_ptr<swarm_base> the_swarm = std::make_shared<swarm>(node_factory, ws_factory, mock_io_context, crypto, "ws://127.0.0.1:0");
+    std::shared_ptr<swarm_base> the_swarm = std::make_shared<swarm>(node_factory, ws_factory, mock_io_context, crypto
+        , "ws://127.0.0.1:0", "my_uuid");
     std::vector<std::shared_ptr<mock_node>> mock_nodes;
 
     std::map<uint16_t, node_meta> nodes;
