595bb9a7f38381079b2782d2eb7614d608c680c5
Yarco Hayduk <yarcoh@gmail.com>
2019-04-26 13:17:14 -0700

    pylib changes

--- py/lib/pylib.py
+++ py/lib/pylib.py
@@ -1,5 +1,8 @@
-from udp.udp_support import *
+from pprint import pprint
+import asyncio
+from lib.udp.udp_support import *
 from build.library import bzpy
+import json
 
 
 class Bluzelle:
@@ -9,10 +12,32 @@ class Bluzelle:
         if (not bzpy.initialize(pub_key, priv_key, "ws://127.0.0.1:50000")):
             raise Exception('Could not run initialize')
 
-    async def has_db(uuid):
+    async def has_db(self, uuid):
         my_port = 1234
         local = await open_local_endpoint('127.0.0.1', my_port)
         resp = bzpy.has_db(uuid)
         cpp_port = resp.get_signal_id(my_port)
 
         data, address = await local.receive() # wait for processing to continue
+        return json.loads(resp.get_result())
+        # await resp
+        # mydb = resp.get_db()
+        # resp = mydb.create("mykey", "myvalue")
+        # await resp
+        # json_resp = resp.get_result()
+        # if (json_resp['result'] == 1):
+        #     pass
+        #     # do stuff
+        # resp = mydb.read("mykey")
+        # await resp
+        # if (json_resp['result'] == 1)
+        #     value = json_resp['value']
+
+
+        # # Create a local UDP enpoint
+
+
+        # print(f"Got {data!r} from {address[0]} port {address[1]}")
+        #
+        # print("CPP result ready? ", response.is_ready())
+        # print("CPP response => ", response.get_result())
--- py/test.py
+++ py/test.py
@@ -1,20 +1,11 @@
-# echo -n "b" | /usr/bin/nc -4u -w0 localhost 1246
 from pprint import pprint
 import sys
 import os
-import asyncio
-import concurrent.futures
-import time
-import uvloop
-import gc
-from socket import *
-
-import asyncio
-import warnings
-
-
 sys.path.extend([os.getcwd()])
 
+import asyncio
+from socket import *
+from lib import pylib
 
 pub_key = "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEiykQ5A02u+02FR1nftxT5VuUdqLO6lvN\n" \
 "oL5aAIyHvn8NS0wgXxbPfpuqUPpytiopiS5D+t2cYzXJn19MQmnl/g=="
@@ -26,29 +17,12 @@ priv_key = "-----BEGIN EC PRIVATE KEY-----\n" \
            "UPpytiopiS5D+t2cYzXJn19MQmnl/g==\n" \
            "-----END EC PRIVATE KEY-----"
 
+uuid = "11111"
 
+async def check_has_db(uuid):
+    bz = pylib.Bluzelle(pub_key, priv_key)
+    res = await bz.has_db(uuid)
+    print("Result:")
+    pprint(res)
 
-
-    # await resp
-    # mydb = resp.get_db()
-    # resp = mydb.create("mykey", "myvalue")
-    # await resp
-    # json_resp = resp.get_result()
-    # if (json_resp['result'] == 1):
-    #     pass
-    #     # do stuff
-    # resp = mydb.read("mykey")
-    # await resp
-    # if (json_resp['result'] == 1)
-    #     value = json_resp['value']
-
-
-    # # Create a local UDP enpoint
-
-
-    # print(f"Got {data!r} from {address[0]} port {address[1]}")
-    #
-    # print("CPP result ready? ", response.is_ready())
-    # print("CPP response => ", response.get_result())
-
-asyncio.run(get_via_socket())
+asyncio.run(check_has_db(uuid))
