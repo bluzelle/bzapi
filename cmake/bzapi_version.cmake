ecf5b2a49a33cfaf110e91b5b198243dc5b7cd31
Ed Bruck <ed.bruck1@gmail.com>
2019-09-04 10:20:38 -0700

    KEP-1462 debian package and published to artifactory (#13)

---
+++ cmake/bzapi_version.cmake
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2019 Bluzelle
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# version for bzapi
+set(PROJECT_VERSION_MAJOR 0 CACHE STRING "Set the project major version")
+set(PROJECT_VERSION_MINOR 0 CACHE STRING "Set the project minor version")
+set(PROJECT_VERSION_PATCH "0-desk" CACHE STRING "Set the project patch version")
+set(PROJECT_VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}" CACHE STRING "")
